1c380c67e793cad427770174c203ea9b4598b9cd
SumitPisal <sumit.pisal22@pccoepune.org>
2024-10-05 13:34:29 +0530

    ihpne check

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -833,7 +833,7 @@ function Homepage() {
 
         <div className='flex flex-col justify-center items-center'>
         <div><img src="./homepage-logo.jpeg" className=' md:w-[500px]  md:h-[500px]  z-50' alt="" /></div>
-        <div><img src="./cyber-font-7.png" className=' md:mt-[-35px] mt-[55px] ml-2 sm:ml-[0] md:ml-[37px] z-50' alt="" /></div>
+        <div><img src="./cyber-font-7.png" className=' md:mt-[-35px] mt-[55px]  sm:ml-[0] md:ml-[37px] z-50' alt="" /></div>
         </div>
         {/* md:-translate-y-[30px] */}
       </div>
@@ -1082,7 +1082,7 @@ function Homepage() {
 
         </div>
       </div>
-      {/* <Footer/> */}
+      <Footer/>
       
     </motion.div>
   );
